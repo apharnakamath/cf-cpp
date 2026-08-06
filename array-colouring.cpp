5072f5285c69823f89c40709f7de18b508f107d3
Apharna Kamath <apharnakamath@gmail.com>
2026-08-07 00:52:21 +0530

    800 pts

---
+++ array-colouring.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+
+        int odd = 0;
+
+        for (int i = 0; i < n; i++) 
+        {
+            int x;
+            cin >> x;
+            if(x%2==1) odd++;
+        }
+	
+	//if only even nums, yes. 
+	//if only odd nums BUT should be even number of odds, yes.
+	//rest all no.
+	
+        if(odd%2==0) cout << "YES"<<endl;
+        else cout << "NO"<<endl;
+    }
+
+    return 0;
+}
