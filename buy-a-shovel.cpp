29b5d3876b1fa38bbdfe685096eae51bacf4438c
Apharna Kamath <apharnakamath@gmail.com>
2026-08-11 00:01:03 +0530

    800 pts

---
+++ buy-a-shovel.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k,r;
+    cin>>k>>r;
+    
+    for(int i=1; i<=10; i++)
+    {
+        int total = k*i;
+        
+        if(total%10==0 || total%10==r)
+        {
+            cout<<i<<endl;
+            break;
+        }
+    }
+}
