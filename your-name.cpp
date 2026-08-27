78ea2bea5ff7b5959c08dbaa0e49b02bdcef73c0
Apharna Kamath <apharnakamath@gmail.com>
2026-08-27 23:36:31 +0530

    800 pts

---
+++ your-name.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        string s,t;
+        cin>>s>>t;
+        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        if(s==t) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
