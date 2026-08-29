3b7d8280085f5d9b279ead3748311edad7c08583
Apharna Kamath <apharnakamath@gmail.com>
2026-08-29 22:54:52 +0530

    800 pts

---
+++ word-on-paper.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string s = "";
+        for(int i=0;i<8;i++)
+        {
+            for(int j=0;j<8;j++)
+            {
+                char c;
+                cin>>c;
+                
+                if(c!='.') s += c;
+            }
+        }
+        
+        cout<<s<<endl;
+    }
+}
