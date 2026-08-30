e90a5cc945bf4155bf27bc1add9fe6222a61fad4
Apharna Kamath <apharnakamath@gmail.com>
2026-08-30 10:09:05 +0530

    800 pts

---
+++ make-it-white.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        string s;
+        
+        cin>>n;
+        cin>>s;
+        
+        int first = -1, last = -1;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='B')
+            {
+                if(first==-1) first = i;
+                last = i;
+            }
+        }
+        
+        cout<<last-first+1<<endl;
+    }
+}
