ef5c96d52c9081cb2daca687650710db39cbe3bc
Apharna Kamath <apharnakamath@gmail.com>
2026-09-04 21:35:28 +0530

    800 pts

---
+++ normal-problem.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]=='p') cout<<'q';
+            else if(s[i]=='q') cout<<'p';
+            else cout<<'w';
+        }
+        
+        cout<<endl;
+    }
+}
